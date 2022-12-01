2ab563d01bb2648fa7f15e0b70e2ff1f1e1a12ee
Daniel Pitfield <danielpitfield1@gmail.com>
2022-12-01 05:36:46 +0000

    Day 1 - part 2

--- Day1/script.ts
+++ Day1/script.ts
@@ -1,18 +1,28 @@
 import { input } from "./input";
 
 const calorieArrays: string[][] = input
-// Split by empty lines
-.split("\n\n")
-// Map each block of values to an array of string values
-.map((x) => x.split("\n"));
+  // Split by empty lines
+  .split("\n\n")
+  // Map each block of values to an array of string values
+  .map((x) => x.split("\n"));
 
 const totals: number[] = [];
 
 for (const item of calorieArrays) {
-    const calorieArray: number[] = item.map(x => parseInt(x));
-    const sum: number = calorieArray.reduce((a,b) => a+b, 0);
-    totals.push(sum);
+  const calorieArray: number[] = item.map((x) => parseInt(x));
+  const sum: number = calorieArray.reduce((a, b) => a + b, 0);
+  totals.push(sum);
 }
 
 const maxTotal: number = Math.max(...totals);
-console.log(maxTotal);
+console.log(maxTotal)
+
+// Largest to smallest
+const sortedTotals = totals.sort((a, b) => {
+  return b - a;
+});
+// Largest three values
+const topThree = sortedTotals.slice(0, 3);
+
+const sumTopThree = topThree.reduce((a, b) => a + b, 0);
+console.log(sumTopThree);
