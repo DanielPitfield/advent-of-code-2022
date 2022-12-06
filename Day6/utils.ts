9f21b18c33d38ebe827aaeb941ba75d79de66032
Daniel Pitfield <danielpitfield1@gmail.com>
2022-12-06 05:22:15 +0000

    refactoring

--- Day6/utils.ts
+++ Day6/utils.ts
@@ -2,10 +2,14 @@ import { input } from "./input";
 
 export function getPacketPosition(markerSize: number): number {
   for (let i = 0; i < input.length - markerSize; i++) {
+    // Take a markerSize sized portion of the input
     const block: string = input.slice(i, i + markerSize);
-    const uniqueValues: string[] = Array.from(new Set([...block]));
+    // How many unique values are within this portion?
+    const numUniqueValues: number = new Set([...block]).size;
 
-    if (uniqueValues.length === markerSize) {
+    // Is every value within the block unique?
+    if (numUniqueValues === markerSize) {
+      // The position of the final letter
       return i + markerSize;
     }
   }
