25f1353bc8f5be82319eda3dd6a0554ba77dcea2
Daniel Pitfield <danielpitfield1@gmail.com>
2022-12-09 20:39:23 +0000

    refactor part 1

--- Day9/part1.ts
+++ Day9/part1.ts
@@ -1,4 +1,30 @@
-import { getVisitedPositions } from "./utils";
+import { Position, instructions, areTouching } from "./utils";
 
-const visitedPositions = getVisitedPositions();
-console.log(visitedPositions.size);
+let headPosition: Position = { xPos: 0, yPos: 0, prevX: 0, prevY: 0 };
+let tailPosition: Position = { xPos: 0, yPos: 0 };
+
+const visitedPositions = new Set();
+
+for (const instruction of instructions) {
+  const [direction, distance] = instruction.split(" ");
+
+  for (let i = 0; i < parseInt(distance); i++) {
+    headPosition.prevX = headPosition.xPos;
+    headPosition.prevY = headPosition.yPos;
+
+    if (direction === "U") headPosition.yPos++;
+    else if (direction === "D") headPosition.yPos--;
+    else if (direction === "L") headPosition.xPos--;
+    else if (direction === "R") headPosition.xPos++;
+
+    if (!areTouching(headPosition, tailPosition)) {
+      tailPosition.xPos = headPosition.prevX;
+      tailPosition.yPos = headPosition.prevY;
+    }
+
+    visitedPositions.add(`${tailPosition.xPos},${tailPosition.yPos}`);
+  }
+}
+
+const numVisitedTiles = visitedPositions.size;
+console.log(numVisitedTiles);
--- Day9/utils.ts
+++ Day9/utils.ts
@@ -1,41 +1,12 @@
 import { input } from "./input";
 
-type Position = { xPos: number; yPos: number; prevX?: number; prevY?: number };
+export const instructions = input.split("\n");
 
-let headPosition: Position = { xPos: 0, yPos: 0, prevX: 0, prevY: 0 };
-let tailPosition: Position = { xPos: 0, yPos: 0 };
+export type Position = { xPos: number; yPos: number; prevX?: number; prevY?: number };
 
-function isTouching(): boolean {
+// Are the head and the tail touching (less than 1 move apart in either vertically or horizontally)
+export function areTouching(headPosition: Position, tailPosition: Position): boolean {
   const xDiff = Math.abs(headPosition.xPos - tailPosition.xPos);
   const yDiff = Math.abs(headPosition.yPos - tailPosition.yPos);
   return xDiff <= 1 && yDiff <= 1;
-}
-
-export function getVisitedPositions() {
-  const visitedPositions = new Set();
-
-  const instructions = input.split("\n");
-
-  for (const instruction of instructions) {
-    const [direction, distance] = instruction.split(" ");
-
-    for (let i = 0; i < parseInt(distance); i++) {
-      headPosition.prevX = headPosition.xPos;
-      headPosition.prevY = headPosition.yPos;
-
-      if (direction === "U") headPosition.yPos++;
-      else if (direction === "D") headPosition.yPos--;
-      else if (direction === "L") headPosition.xPos--;
-      else if (direction === "R") headPosition.xPos++;
-
-      if (!isTouching()) {
-        tailPosition.xPos = headPosition.prevX;
-        tailPosition.yPos = headPosition.prevY;
-      }
-
-      visitedPositions.add(`${tailPosition.xPos},${tailPosition.yPos}`);
-    }
-  }
-
-  return visitedPositions;
 }
