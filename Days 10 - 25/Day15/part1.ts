e3c2ea94fae00524761bee5e0be36c5c82c4f4a6
Daniel Pitfield <danielpitfield1@gmail.com>
2022-12-16 12:20:05 +0000

    Day 15 - sensor and beacon positions

---
+++ Days 10 - 25/Day15/part1.ts
@@ -0,0 +1,25 @@
+import { Position } from "../../Days 1 - 9/Day9/utils";
+import { input } from "./input";
+import { calculateManhattanDistance, getExcludedPositions } from "./utils";
+
+const allExcludedPositions: Position[][] = [];
+
+const lines = input.split("\n");
+
+for (const line of lines) {
+  const [sensorDescription, beaconDescription] = line.split(":");
+
+  const sensorXPos = sensorDescription.split(" ")[2].split("=")[1];
+  const sensorYPos = sensorDescription.split(" ")[3].split("=")[1];
+  const sensorPosition: Position = { xPos: parseInt(sensorXPos), yPos: parseInt(sensorYPos) };
+
+  const beaconXPos = beaconDescription.split(" ")[5].split("=")[1];
+  const beaconYPos = beaconDescription.split(" ")[6].split("=")[1];
+  const beaconPosition: Position = { xPos: parseInt(beaconXPos), yPos: parseInt(beaconYPos) };
+
+  const distance = calculateManhattanDistance(sensorPosition, beaconPosition);
+
+  const excludedPositions: Position[] = getExcludedPositions(sensorPosition, distance);
+
+  allExcludedPositions.push(excludedPositions);
+}
---
+++ Days 10 - 25/Day15/utils.ts
@@ -0,0 +1,31 @@
+import { Position } from "../../Days 1 - 9/Day9/utils";
+
+// The Manhattan distance between two points
+export function calculateManhattanDistance(point1: Position, point2: Position): number {
+  const diffX = Math.abs(point1.xPos - point2.xPos);
+  const diffY = Math.abs(point1.yPos - point2.yPos);
+
+  return diffX + diffY;
+}
+
+// What positions can't have another sensor (when given a sensor position and the distance around it where another sensor cannot be)?
+export function getExcludedPositions(sensorPosition: Position, distance: number): Position[] {
+  const excludedPositions: Position[] = [];
+
+  // From the row (which is distance number of rows above the sensor position) to the row (which is distance number of rows below the sensor position)
+  for (let yPos = sensorPosition.yPos - distance; yPos <= sensorPosition.yPos + distance; yPos++) {
+    // How many rows away from the sensor position?
+    const rowDiff = Math.abs(sensorPosition.yPos - yPos) - distance + 1;
+    // How many positions will be excluded on this row?
+    const numExcludedPositionsForRow = 2 * rowDiff - 1;
+    // Centered, half on left, half on right
+    const distanceEitherWay = Math.floor((numExcludedPositionsForRow - 1) / 2);
+
+    for (let xPos = sensorPosition.xPos - distanceEitherWay; xPos <= sensorPosition.xPos + distanceEitherWay; xPos++) {
+      const excludedPosition: Position = { xPos: xPos, yPos: yPos };
+      excludedPositions.push(excludedPosition);
+    }
+  }
+
+  return excludedPositions;
+}
