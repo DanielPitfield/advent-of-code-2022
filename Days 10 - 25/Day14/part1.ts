754ff7874b5a7a743f7b77226b93b0a3f8f2cfb4
Daniel Pitfield <danielpitfield1@gmail.com>
2022-12-16 12:19:50 +0000

    Day 14 - visualisation

--- Days 10 - 25/Day14/part1.ts
+++ Days 10 - 25/Day14/part1.ts
@@ -1,3 +1,46 @@
+import { Position } from "../../Days 1 - 9/Day9/utils";
 import { getRockWalls } from "./utils";
 
-const rockWalls = getRockWalls();
+const startingSandPosition: Position = { xPos: 500, yPos: 0 };
+const rockWalls: Position[] = getRockWalls().flat();
+
+const MIN_XPOS = rockWalls.reduce((min, x) => {
+  if (x.xPos < min) {
+    min = x.xPos;
+  }
+  return min;
+}, 0);
+
+const MAX_XPOS = rockWalls.reduce((max, x) => {
+  if (x.xPos > max) {
+    max = x.xPos;
+  }
+  return max;
+}, 0);
+
+const MIN_YPOS = rockWalls.reduce((min, x) => {
+  if (x.yPos < min) {
+    min = x.yPos;
+  }
+  return min;
+}, 0);
+
+const MAX_YPOS = rockWalls.reduce((max, x) => {
+  if (x.yPos > max) {
+    max = x.yPos;
+  }
+  return max;
+}, 0);
+
+const Grid = new Array(MAX_YPOS - MIN_YPOS).fill(".").map((row) => new Array(MAX_XPOS - MIN_XPOS).fill("."));
+
+for (let y = 0; y < MAX_YPOS; y++) {
+  for (let x = 0; x < MAX_XPOS; x++) {
+    console.log(`${x},${y}`);
+    if (rockWalls.some((position) => position.xPos === x + MIN_XPOS && position.yPos === y + MIN_YPOS)) {
+      Grid[y][x] = "#";
+    }
+  }
+}
+
+console.log(Grid.map((x) => x.join("")).join("\n"));
