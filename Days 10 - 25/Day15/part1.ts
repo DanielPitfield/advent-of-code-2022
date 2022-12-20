7106c8e2494e79306f92f2d1119a47c848647a58
Daniel Pitfield <danielpitfield1@gmail.com>
2022-12-20 04:39:42 +0000

    Refactor part 1

--- Days 10 - 25/Day15/part1.ts
+++ Days 10 - 25/Day15/part1.ts
@@ -1,37 +1,9 @@
-import { Position } from "../../Days 1 - 9/Day9/utils";
-import { input } from "./input";
-import { calculateManhattanDistance, getExcludedPositions } from "./utils";
-
-// Array to hold arrays of the x positions of excluded positions (which have a yPos of the targetRowNumber)
-const allExcludedPositions: number[][] = [];
-
-const lines = input.split("\n");
+import { getExcludedPositions } from "./utils";
 
 // What is the row number to check?
 const targetRowNumber = 2000000;
+const excludedPositions = getExcludedPositions(targetRowNumber);
 
-// Parsing the sensor and beacon position
-for (const line of lines) {
-  const [sensorDescription, beaconDescription] = line.split(":");
-
-  const sensorXPos = sensorDescription.split(" ")[2].split("=")[1];
-  const sensorYPos = sensorDescription.split(" ")[3].split("=")[1];
-  const sensorPosition: Position = { xPos: parseInt(sensorXPos), yPos: parseInt(sensorYPos) };
-
-  const beaconXPos = beaconDescription.split(" ")[5].split("=")[1];
-  const beaconYPos = beaconDescription.split(" ")[6].split("=")[1];
-  const beaconPosition: Position = { xPos: parseInt(beaconXPos), yPos: parseInt(beaconYPos) };
-
-  // The Manhattan distance between the sensor and the beacon
-  const distance = calculateManhattanDistance(sensorPosition, beaconPosition);
-
-  // What positions can there not be another sensor (because the current sensor must be the closest to the beacon)?
-  const excludedPositions = getExcludedPositions(sensorPosition, distance, targetRowNumber);
-
-  allExcludedPositions.push(excludedPositions);
-}
-
-// How many positions on this row cannot be the position of a sensor?
-const numTargetExcludedPositions = new Set(allExcludedPositions.flat());
-// Don't count the sensor on the target row (therefore subtract 1)
-console.log(numTargetExcludedPositions.size - 1);
+// Subtract 1 as the sensor on the row doesn;t count as an excluded position
+const numExcludedPositions = excludedPositions.length - 1;
+console.log(numExcludedPositions);
--- Days 10 - 25/Day15/utils.ts
+++ Days 10 - 25/Day15/utils.ts
@@ -1,4 +1,5 @@
 import { Position } from "../../Days 1 - 9/Day9/utils";
+import { input } from "./input";
 
 // The Manhattan distance between two points
 export function calculateManhattanDistance(point1: Position, point2: Position): number {
@@ -9,18 +10,36 @@ export function calculateManhattanDistance(point1: Position, point2: Position): 
 }
 
 // What positions (that have a yPos of the targetRowNumber) cannot be the position of a sensor?
-export function getExcludedPositions(sensorPosition: Position, distance: number, targetRowNumber: number): number[] {
+export function getExcludedPositions(targetRowNumber: number): number[] {
+  const lines = input.split("\n");
+
   // The xPos values for the excluded positions
-  const targetRowXPositions: Set<number> = new Set();
+  const excludedPositions: Set<number> = new Set();
 
-  for (let xPos = sensorPosition.xPos - distance; xPos <= sensorPosition.xPos + distance; xPos++) {
-    const currentlyScannedPosition: Position = { xPos: xPos, yPos: targetRowNumber };
+  // Parsing the sensor and beacon position
+  for (const line of lines) {
+    const [sensorDescription, beaconDescription] = line.split(":");
 
-    // Within the distance between the sensor and beacon
-    if (calculateManhattanDistance(sensorPosition, currentlyScannedPosition) <= distance) {
-      targetRowXPositions.add(xPos);
+    const sensorXPos = sensorDescription.split(" ")[2].split("=")[1];
+    const sensorYPos = sensorDescription.split(" ")[3].split("=")[1];
+    const sensorPosition: Position = { xPos: parseInt(sensorXPos), yPos: parseInt(sensorYPos) };
+
+    const beaconXPos = beaconDescription.split(" ")[5].split("=")[1];
+    const beaconYPos = beaconDescription.split(" ")[6].split("=")[1];
+    const beaconPosition: Position = { xPos: parseInt(beaconXPos), yPos: parseInt(beaconYPos) };
+
+    // The Manhattan distance between the sensor and the beacon
+    const distance = calculateManhattanDistance(sensorPosition, beaconPosition);
+
+    for (let xPos = sensorPosition.xPos - distance; xPos <= sensorPosition.xPos + distance; xPos++) {
+      const currentlyScannedPosition: Position = { xPos: xPos, yPos: targetRowNumber };
+
+      // Within the distance between the sensor and beacon?
+      if (calculateManhattanDistance(sensorPosition, currentlyScannedPosition) <= distance) {
+        excludedPositions.add(xPos);
+      }
     }
   }
 
-  return Array.from(targetRowXPositions);
+  return Array.from(excludedPositions);
 }
