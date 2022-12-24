6cc34d176843d0fdc9ac7a8326f35b9c4c764f44
Daniel Pitfield <danielpitfield1@gmail.com>
2022-12-24 02:17:20 +0000

    Day 21 - unsafe eval of strings

--- Days 10 - 25/Day21/part1.ts
+++ Days 10 - 25/Day21/part1.ts
@@ -1,36 +1,30 @@
-import { operatorMappings } from "../Day11/utils";
-import { input } from "./input";
-import { isJobValuePrimitive } from "./utils";
+import {
+  evaluateMonkeyJobValues,
+  initialMonkeyJobs,
+  isAnotherMonkeyValue,
+  MonkeyJob,
+  replaceMonkeyJobValues,
+} from "./utils";
 
-type MonkeyJob = { name: string; value: string };
+function getFinalRootValue(): number | null {
+  let currentMonkeyJobs: MonkeyJob[] = initialMonkeyJobs.slice();
+  let finalRootValue: string = "abcd";
 
-const monkeyJobs: MonkeyJob[] = input.split("\n").map((monkey) => {
-  const [name, value] = monkey.split(":");
-  return { name, value };
-});
+  while (isAnotherMonkeyValue(finalRootValue)) {
+    const replaced = replaceMonkeyJobValues(currentMonkeyJobs);
+    const evaluated = evaluateMonkeyJobValues(replaced);
+    currentMonkeyJobs = evaluated;
 
-const resolvedMonkeyJobs: MonkeyJob[] = monkeyJobs.map((monkeyJob) => {
-  // The value of the job is already just an integer
-  if (isJobValuePrimitive(monkeyJob.value)) {
-    return monkeyJob;
+    const currentRootValue = currentMonkeyJobs.find((monkey) => monkey.name === "root")?.value;
+
+    if (!currentRootValue) {
+      return null;
+    }
+
+    finalRootValue = currentRootValue;
   }
 
-  // Otherwise, the value is still an expression
-  let [_, firstValue, operatorSymbol, secondValue] = monkeyJob.value.split(/[.\*+-/_]/);
+  return parseInt(finalRootValue);
+}
 
-  // Both the operands are primitive and the expression can be resolved
-  if (isJobValuePrimitive(firstValue) && isJobValuePrimitive(secondValue)) {
-    const newValue: string =
-      operatorMappings
-        .find((x) => x.symbol === operatorSymbol)
-        ?.function(parseInt(firstValue), parseInt(secondValue))
-        .toString() ?? "";
-
-    return { ...monkeyJob, value: newValue };
-  }
-
-  // TODO: Values aren't primitive, try and look for their value
-
-});
-
-console.log(resolvedMonkeyJobs);
+console.log(getFinalRootValue());
--- Days 10 - 25/Day21/utils.ts
+++ Days 10 - 25/Day21/utils.ts
@@ -1,4 +1,51 @@
-// Is the monkey job value simply a value and not an expression?
-export function isJobValuePrimitive(value: string) {
-  return parseInt(value).toString() === value;
+import { input } from "./input";
+
+export type MonkeyJob = { name: string; value: string };
+
+export const initialMonkeyJobs: MonkeyJob[] = input.split("\n").map((monkey) => {
+  const [name, value] = monkey.split(": ");
+  return { name, value };
+});
+
+export function isAnotherMonkeyValue(value: string): boolean {
+  const alphabetString: string = "abcdefghijklmnopqrstuvwxyz";
+  const lowercaseArray: string[] = alphabetString.split("");
+
+  return value.split("").some((letter) => lowercaseArray.includes(letter));
+}
+
+// Map over the monkeyJobs replacing any string parts of the job value with known values
+export function replaceMonkeyJobValues(monkeyJobs: MonkeyJob[]): MonkeyJob[] {
+  return monkeyJobs.map((monkeyJob) => {
+    // The operands and operators of the monkey job's value
+    const valueParts: string[] = monkeyJob.value.split(" ");
+
+    const replacedParts: string[] = valueParts.map((part) => {
+      // A pointer to another monkey's value
+      if (isAnotherMonkeyValue(part)) {
+        // Replace with the value of the monkey that has the name of the part
+        return monkeyJobs.find((monkeyJob) => monkeyJob.name === part)?.value ?? part;
+      }
+
+      return part;
+    });
+
+    // Put expression back together again
+    return { ...monkeyJob, value: replacedParts.join(" ") };
+  });
+}
+
+export function evaluateMonkeyJobValues(monkeyJobs: MonkeyJob[]): MonkeyJob[] {
+  return monkeyJobs.map((monkeyJob) => {
+    const valueParts: string[] = monkeyJob.value.split(" ");
+
+    // There are no parts which point to the values of other monkeys
+    if (valueParts.every((part) => !isAnotherMonkeyValue(part))) {
+      // Evaluate the expression
+      const newValue = parseInt(eval(monkeyJob.value)).toString();
+      return { ...monkeyJob, value: newValue };
+    }
+
+    return monkeyJob;
+  });
 }
