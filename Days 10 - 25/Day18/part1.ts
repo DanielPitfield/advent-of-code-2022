bf6075e087f23c1fcbfeb4d22f9e7cb3d1697716
Daniel Pitfield <danielpitfield1@gmail.com>
2022-12-27 06:03:24 +0000

    export cubes

--- Days 10 - 25/Day18/part1.ts
+++ Days 10 - 25/Day18/part1.ts
@@ -1,35 +1,10 @@
-import { input } from "./input";
+import { allCubes, getNumberOfSidesExposed } from "./utils";
 
-type Cube = { xPos: number; yPos: number; zPos: number };
-
-const allCubes: Cube[] = input.split("\n").map((line) => {
-  const [xPos, yPos, zPos] = line.split(",").map((x) => parseInt(x.trim()));
-  return { xPos, yPos, zPos };
-});
-
-function getNumberOfSidesExposed(cube: Cube): number {
-  // The adjacent positions of the cube
-  const side1 = { ...cube, xPos: cube.xPos + 1 };
-  const side2 = { ...cube, xPos: cube.xPos - 1 };
-  const side3 = { ...cube, yPos: cube.yPos + 1 };
-  const side4 = { ...cube, yPos: cube.yPos - 1 };
-  const side5 = { ...cube, zPos: cube.zPos + 1 };
-  const side6 = { ...cube, zPos: cube.zPos - 1 };
-
-  const adjacentSides: Cube[] = [side1, side2, side3, side4, side5, side6];
-
-  // How many of these adjacent sides are found within allCubes?
-  const numCoveredSides: number = allCubes
-    .map((cube) => JSON.stringify(cube))
-    .filter((cube) => adjacentSides.map((cube) => JSON.stringify(cube)).includes(cube)).length;
-
-  // The number of exposed sides is the total number of sides minus the number of covered sides
-  return adjacentSides.length - numCoveredSides;
-}
-
+// Map each sube to its exposed surface area (each exposed side is 1 unit)
 const allCubesExposedSurfaceAreas: number[] = allCubes.map((cube) => {
   return getNumberOfSidesExposed(cube);
 });
 
+// Sum the surface area of every cube together
 const totalSurfaceArea: number = allCubesExposedSurfaceAreas.reduce((a, b) => a + b, 0);
 console.log(totalSurfaceArea);
