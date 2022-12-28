35d0652bd23bfff2be66c9f57a1da97ea1a73641
Daniel Pitfield <danielpitfield1@gmail.com>
2022-12-28 01:09:28 +0000

    get adjacent cubes function

--- Days 10 - 25/Day18/utils.ts
+++ Days 10 - 25/Day18/utils.ts
@@ -8,7 +8,7 @@ export const allCubes: Cube[] = input.split("\n").map((line) => {
   return { xPos, yPos, zPos };
 });
 
-export function getNumberOfSidesExposed(cube: Cube): number {
+export function getAllAdjacentCubes(cube: Cube): Cube[] {
   // The adjacent positions of the cube
   const side1: Cube = { ...cube, xPos: cube.xPos + 1 };
   const side2: Cube = { ...cube, xPos: cube.xPos - 1 };
@@ -17,43 +17,21 @@ export function getNumberOfSidesExposed(cube: Cube): number {
   const side5: Cube = { ...cube, zPos: cube.zPos + 1 };
   const side6: Cube = { ...cube, zPos: cube.zPos - 1 };
 
-  const adjacentSides: Cube[] = [side1, side2, side3, side4, side5, side6];
+  return [side1, side2, side3, side4, side5, side6];
+}
+
+export function getNumberOfSidesExposed(cube: Cube): number {
+  const adjacentCubes: Cube[] = getAllAdjacentCubes(cube);
 
   // How many of these adjacent sides are found within allCubes?
   const numCoveredSides: number = allCubes
     .map((cube) => JSON.stringify(cube))
-    .filter((cube) => adjacentSides.map((cube) => JSON.stringify(cube)).includes(cube)).length;
+    .filter((cube) => adjacentCubes.map((cube) => JSON.stringify(cube)).includes(cube)).length;
 
   // The number of exposed sides is the total number of sides minus the number of covered sides
-  return adjacentSides.length - numCoveredSides;
+  return adjacentCubes.length - numCoveredSides;
 }
 
-// Has to be within the occupied area and not on the edges (hence +1 for minimums and -1 for maximums)
-export function getNumAirDroplets(): number {
-  const minX: number = Math.min(...allCubes.map((cube) => cube.xPos)) + 1;
-  const maxX: number = Math.max(...allCubes.map((cube) => cube.xPos)) - 1;
-
-  const minY: number = Math.min(...allCubes.map((cube) => cube.yPos)) + 1;
-  const maxY: number = Math.max(...allCubes.map((cube) => cube.yPos)) - 1;
-
-  const minZ: number = Math.min(...allCubes.map((cube) => cube.zPos)) + 1;
-  const maxZ: number = Math.max(...allCubes.map((cube) => cube.zPos)) - 1;
-
-  let counter = 0;
-
-  // Loop through all the positions (of the cubular space from the origin to the furthest cube)
-  for (let xPos = minX; xPos <= maxX; xPos++) {
-    for (let yPos = minY; yPos <= maxY; yPos++) {
-      for (let zPos = minZ; zPos <= maxZ; zPos++) {
-        // The current position to check
-        const cubeToCheck: Cube = { xPos, yPos, zPos };
-        // Not a cube occupied, but is fully covered
-        if (!allCubes.includes(cubeToCheck) && getNumberOfSidesExposed(cubeToCheck) === 0) {
-          counter++;
-        }
-      }
-    }
-  }
-
-  return counter;
+export function isCubeWithin(cubeArray: Cube[], cubeToCheck: Cube): boolean {
+  return cubeArray.map((cube) => JSON.stringify(cube)).some((cube) => cube === JSON.stringify(cubeToCheck));
 }
