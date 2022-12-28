ebcd9d8f43ec123fd66e32c3d8987e6cb04b9ebd
Daniel Pitfield <danielpitfield1@gmail.com>
2022-12-28 03:09:30 +0000

    use existing arrays

--- Days 10 - 25/Day18/part2.ts
+++ Days 10 - 25/Day18/part2.ts
@@ -5,20 +5,15 @@ import {
   getMaximumPosition,
   getMinimumPosition,
   getNumAdjacentCubesPresent,
+  isCubeWithin,
 } from "./utils";
 
 // The minimum and maximum values allowed for the cube to be an air droplet
 const MIN = getMinimumPosition();
 const MAX = getMaximumPosition();
 
-const allCubesSet: Set<string> = new Set(
-  allCubes.map((cube) => {
-    return `${cube.xPos},${cube.yPos},${cube.zPos}`;
-  })
-);
-
 let exteriorSurfaceArea: number = 0;
-const visitedCubePositions: Set<string> = new Set();
+const visitedCubes: Cube[] = [];
 
 const origin: Cube = { xPos: 0, yPos: 0, zPos: 0 };
 let queue: Cube[] = [origin];
@@ -26,31 +21,30 @@ let queue: Cube[] = [origin];
 // Until the queue is empty
 while (queue.length > 0) {
   const currentCube: Cube = queue.shift()!;
-  const currentCubeStringified = `${currentCube.xPos},${currentCube.yPos},${currentCube.zPos}`;
 
   // Already processed this cube
-  if (visitedCubePositions.has(currentCubeStringified)) {
+  if (isCubeWithin(visitedCubes, currentCube)) {
     continue;
   }
 
   // The cube is within the interior of the droplet structure
-  if (allCubesSet.has(currentCubeStringified)) {
+  if (isCubeWithin(allCubes, currentCube)) {
     continue;
   }
 
   // Cube is out of bounds
-  if ([currentCube.xPos, currentCube.yPos, currentCube.zPos].some(position => position < MIN || position > MAX))  {
+  if ([currentCube.xPos, currentCube.yPos, currentCube.zPos].some((position) => position < MIN || position > MAX)) {
     continue;
   }
 
   // This cube is an air droplet, add it's surface area to the sum
   exteriorSurfaceArea += getNumAdjacentCubesPresent(currentCube);
 
-  visitedCubePositions.add(currentCubeStringified);
+  visitedCubes.push(currentCube);
 
   const adjacentCubes = getAllAdjacentCubes(currentCube);
   // Push the adjacent cubes of the current cube to the queue
   queue = queue.concat(adjacentCubes);
 }
 
-console.log(exteriorSurfaceArea)
+console.log(exteriorSurfaceArea);
